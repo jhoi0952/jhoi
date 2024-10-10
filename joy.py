4ed117d950bbc2320d5ced2c1066fb013bb7b945
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 <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>
2024-10-10 22:55:56 +0100

    turples and list

---
+++ joy.py
@@ -0,0 +1,55 @@
+print(str(number))#output 78
+
+
+my_name = 'joy'
+age = 10000
+place = 'world'
+job = 'student'
+school = "spring sch"
+alphabet = 'R'
+num = '88'
+id ='9'
+hobbies ="sleeping"
+
+num1 = 10 
+num2 = 20
+print(num1 + num2)
+sum = num1 + num2
+minus = num1 - num2
+print(sum)
+multiply = num1 * num2
+print(sum)
+divide = num1 / num2
+print(sum)
+
+
+float_num = 20.0 #number
+
+string_value ="my name is joy"
+print(string_value)
+s =  " and  favour"
+print(s+ string_value)
+print("my name is joy" + s)
+string_value.upper
+print(string_value)
+print(string_value.count('a'))
+print(string_value.replace("j","l"))
+print(string_value.upper())
+print(string_value .lower())
+print(string_value.isupper())
+print(string_value.islower())
+print(string_value.__len__())
+
+
+#inputs
+name = input("what is your name?")
+print("your name is"+ name)
+
+
+#simple calculator to get addition ans subtraction
+number1 = int(input("input any number"))
+number2 = int(input("input another number"))
+add = number1 + number2
+sub = number2 - number1
+print(f"the sum of your numbers are {add} ")
+print(f" the subtraction of your number is  {sub}" )
